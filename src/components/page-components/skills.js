2ac57161d5e73696b5d9218fe66230d2d55f3665
lilifernandez91 <lilibetfdez@gmail.com>
2023-06-27 12:21:06 +0200

    add projects

--- src/components/page-components/skills.js
+++ src/components/page-components/skills.js
@@ -13,7 +13,7 @@ const SkillsComponent = () => {
                         Eficiente en el uso de herramientas como Slack, GitHub, VS Code y Terminal, lo que me permite trabajar de manera efectiva y optimizar el proceso de desarrollo.
                     </p>
                     <p>
-                        Apasionada por la tecnología y la programación, lo que me motiva a estar siempre en constante aprendizaje y en búsqueda de soluciones creativas e innovadoras para cada proyecto.
+                        Apasionada por la tecnología y la programación, lo que me motiva a estar siempre en constante aprendizaje y en búsqueda de soluciones creativas para cada proyecto.
                     </p>
                     <p>
                         Me encanta trabajar en equipo de manera colaborativa y mantener una comunicación constante para lograr los objetivos del proyecto.
